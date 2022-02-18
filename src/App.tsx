91c64aa0e37aa7c3ff385e457d01e0b856b2be77
ShiroKatake <tgkhang262@gmail.com>
2022-02-19 01:29:20 +1100

    UPDATE: Output the most expensive purchase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import logo from "./logo.svg";
 import Papa from "papaparse";
 import "./App.css";
 
 export const App = () => {
   const [csv, setCsv] = useState<FileList | null>();
-  const [data, setData] = useState({});
+  const [data, setData] = useState<any>(null);
+  const [output, setOutput] = useState<any>({
+    mostExpensive: {},
+  });
 
+  const initialRender = useRef(true);
   useEffect(() => {
-    console.log("Finished:", data);
+    if (initialRender.current) {
+      initialRender.current = false;
+    } else {
+      console.log("Finished:", data);
+      setOutput((prevState: any) => ({
+        ...prevState,
+        mostExpensive: findMostExpensive(data),
+      }));
+    }
   }, [data]);
 
   const handleOnSubmit = () => {
@@ -20,6 +32,10 @@ export const App = () => {
         },
       });
     }
+  };
+
+  const findMostExpensive = (resultArray: any[]) => {
+    return resultArray.sort((a, b) => a.Amount - b.Amount)[0];
   };
 
   return (
@@ -36,8 +52,14 @@ export const App = () => {
           }}
         />
         <button type="button" onClick={handleOnSubmit}>
-          Submit
+          Analyze
         </button>
+        {data && (
+          <p>
+            Your most expensive purchase was "{output.mostExpensive.Name}" at{" "}
+            {`$${Math.abs(output.mostExpensive.Amount)}`}
+          </p>
+        )}
       </header>
     </div>
   );

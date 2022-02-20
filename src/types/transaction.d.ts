3d8d40d293407acb837bb7d6070eb77f06f9c177
ShiroKatake <tgkhang262@gmail.com>
2022-02-20 13:10:06 +1100

    REMOVE: Processed date

--- src/types/transaction.d.ts
+++ src/types/transaction.d.ts
@@ -1,7 +1,6 @@
 //prettier-ignore
 export interface Transaction {
   date: string;       // Date the transaction was made
-  processed: string;  // Date the money was processed
   name: string;       // Transaction name
   amount: number;     // Amount debited or credited
   balance: number;    // Account balance

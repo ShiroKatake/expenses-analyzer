4e17345312b61b33c3cabd4f666eb7c46abf427a
ShiroKatake <tgkhang262@gmail.com>
2022-02-20 13:10:46 +1100

    UPDATE: Deep copied transaction data before transforming it

--- src/analysis/findMostExpensivePurchase.tsx
+++ src/analysis/findMostExpensivePurchase.tsx
@@ -3,7 +3,8 @@ import { stringToDate } from "../utils/stringToDate";
 
 // Might need to convert this to component instead of function
 export const findMostExpensivePurchase = (resultArray: Transaction[]) => {
-  const purchase = resultArray.sort((a, b) => a.amount - b.amount)[0];
+  const deepCopy = JSON.parse(JSON.stringify(resultArray)) as Transaction[];
+  const purchase = deepCopy.sort((a, b) => a.amount - b.amount)[0];
   const date = stringToDate(purchase.date);
   const amount = `$${Math.abs(purchase.amount)}`;
   return (
